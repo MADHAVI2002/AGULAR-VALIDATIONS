c01eb9c79336a365b49c1198234b6a8573e6838a
MADHAVI <madhavigollapati242@gmail.com>
2022-07-19 21:48:11 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, ɵɵpureFunction1 } from '@angular/core'
+import {  FormGroup,FormControl,Validators } from '@angular/forms'
+//import {Student} from './student'
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector:"app-root",
+templateUrl:"app.component.html",
+//styleUrls: ['app.component.css'],
 })
-export class AppComponent {
-  title = 'appPROJECT';
-}
+
+
+export  class AppComponent{
+    loginForm=new FormGroup({
+        user:new FormControl('',[Validators.required,
+        Validators.minLength(5)]),
+        password:new FormControl('',
+        [Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(8), ]),
+      
+      email:new FormControl('',[Validators.email]),
+      })
+        loginUser(){
+          console.log(this.loginForm.value)
+        }
+        get user(){
+          return this.loginForm.get('user')
+        }
+        get password(){
+          return this.loginForm.get('password')
+        }
+        
+        get email(){
+          return this.loginForm.get('email')
+        }
+ }
+ 
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import {NgModule} from '@angular/core'
+import { AppComponent } from './app.component'
+import { BrowserModule } from '@angular/platform-browser'
+import { ReactiveFormsModule } from '@angular/forms';
+//import {FormsModule } from "@angular/forms"
+//import { DeclarationListEmitMode } from '@angular/compiler'
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports:[BrowserModule,ReactiveFormsModule],
+    declarations:[AppComponent],
+    providers: [],
+    bootstrap:[AppComponent]
+
 })
-export class AppModule { }
+
+
+
+
+
+export class AppModule{
+
+}
